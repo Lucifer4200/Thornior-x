d1bbaa23caed463da4539bbded719b8d6cffea59
yeasin2448 <ayeasin108@gmail.com>
2021-03-30 22:00:16 +0600

    1st update

---
+++ assets/js/function.js
@@ -0,0 +1,133 @@
+(function($){
+	'use strict';
+
+    //menu side bar
+    $('.mobile-bar').on('click', function(){
+        $('body,header,.mobile-bar,.main-menu').toggleClass('active');
+    });
+
+    $('.crose-bar').on('click', function(){
+        $('body,header,.mobile-bar,.main-menu').removeClass('active');
+    });
+
+    // menu icon-related
+    $(".list-item>li>.sub-menu").parent("li").children("a").addClass("icon-down");
+
+    //mobile drodown menu display
+	$('ul.menu-list li a').on('click', function(e) {
+        var element = $(this).parent('li');
+        if (element.hasClass('open')) {
+            element.removeClass('open');
+            element.find('li').removeClass('open');
+            element.find('ul').slideUp(500,"swing");
+        }
+        else {
+            element.addClass('open');
+            element.children('ul').slideDown(500,"swing");
+            element.siblings('li').children('ul').slideUp(500,"swing");
+            element.siblings('li').removeClass('open');
+            element.siblings('li').find('li').removeClass('open');
+            element.siblings('li').find('ul').slideUp(500,"swing");
+        }
+	}); 
+
+
+    // drop down menu width overflow problem fix
+    $('ul').parent().on('hover', function() {
+        var menu = $(this).find("ul");
+        var menupos = $(menu).offset();
+        if (menupos.left + menu.width() > $(window).width()) {
+            var newpos = -$(menu).width();
+            menu.css({ left: newpos });    
+        }
+    });
+    // Theme switcher
+    $(document).ready(function(){
+        $(".theme-switcher").click(function(){
+        $(this).text(function(i, v){
+        return v === 'light' ? 'dark' : 'light'
+        });
+        });
+    });
+
+    (function() {
+    $('.theme-switcher').click(function() {
+        $('#theme').toggleClass('dark');
+    });
+    })();
+
+     // Scroll To Top 
+    var scrollTop = $(".scrollToTop");
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() < 500) {
+        scrollTop.removeClass("active");
+        } else {
+        scrollTop.addClass("active");
+        }
+    });
+    
+    //Click event to scroll to top
+    $('.scrollToTop').on('click', function () {
+        $('html, body').animate({
+        scrollTop: 0
+        }, 500);
+        return false;
+    });
+
+    // banner-slider activation
+    var swiper = new Swiper('.banner-slider', {
+        slidesPerView: 1.5,
+        spaceBetween: 0,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 1.5,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            1024: {
+                slidesPerView: 3,
+            },
+        }
+    });
+
+    //sponser slider activation
+    var swiper = new Swiper('.categori-slider', {
+        slidesPerView: 2,
+        speed:1000,
+        autoplay: {
+            delay: 5000,
+            disableOnInteraction: true,
+        },
+        loop: true,
+        freeMode: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            425: {
+                slidesPerView: 2,
+            },
+            576: {
+                slidesPerView: 2,
+            },
+            768: {
+                slidesPerView: 4,
+            },
+            1024: {
+                slidesPerView: 6,
+            }
+        }
+    });
+
+
+})(jQuery);
